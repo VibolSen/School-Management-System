510dc307c353304c1b1ebb83024440055450285e
Vibol Sen <144869855+VibolSen@users.noreply.github.com>
2025-09-09 14:16:57 +0700

    Add

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/nav/Navbar";
 import Footer from "@/components/nav/Footer";
-import HomePage from "@/components/page/HomePage";
+import HomePage from "@/components/pages/HomePage";
 
 export default function Home() {
   return (
